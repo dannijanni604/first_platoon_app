b9c33c81756ea054960637b064b67c9fb2b809af
mianazeemdaula <mianazeemdaula@gmail.com>
2022-12-07 23:02:08 +0500

    wrong

--- lib/controllers/manage_controller.dart
+++ lib/controllers/manage_controller.dart
@@ -1,8 +1,9 @@
 import 'dart:developer';
 import 'dart:io';
 import 'package:dio/dio.dart';
+import 'package:first_platoon/core/components/snackbar.dart';
 import 'package:get/get.dart';
-import 'package:open_file/open_file.dart';
+import 'package:open_file_plus/open_file_plus.dart';
 import 'package:path_provider/path_provider.dart';
 
 class ManageController extends GetxController {
@@ -21,12 +22,16 @@ class ManageController extends GetxController {
   // }
 
   Future downloadAndOpenFile({String? url, String? fileName}) async {
-    final file = await downloadFile(url, fileName);
-    if (file == null) return null;
-    log('Path :: ${file.path}');
-    OpenFile.open(
-      file.path,
-    );
+    try {
+      final file = await downloadFile(url, fileName);
+      if (file == null) return null;
+      log('Path :: ${file.path}');
+      await OpenFile.open(
+        file.path,
+      );
+    } catch (e) {
+      kerrorSnackbar(message: "Somthing went wrong");
+    }
   }
 
   Future<File?> downloadFile(String? url, String? fileName) async {
